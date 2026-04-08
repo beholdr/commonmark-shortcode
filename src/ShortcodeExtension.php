ea6b52326944d58007d1e32c82fe357e5a18df2f
Alexander Shabunevich <loonar@gmail.com>
2026-04-08 10:49:26 +0300

    feat: allow to set extension priority

--- src/ShortcodeExtension.php
+++ src/ShortcodeExtension.php
@@ -7,12 +7,15 @@ use League\CommonMark\Extension\ExtensionInterface;
 
 class ShortcodeExtension implements ExtensionInterface
 {
-    public function __construct(private ShortcodeRegistry $registry) {}
+    public function __construct(
+        private ShortcodeRegistry $registry,
+        private int $priority = 99,
+    ) {}
 
     public function register(EnvironmentBuilderInterface $environment): void
     {
         $environment
-            ->addInlineParser(new ShortcodeParser($this->registry), 99)
+            ->addInlineParser(new ShortcodeParser($this->registry), $this->priority)
             ->addRenderer(ShortcodeNode::class, new ShortcodeRenderer($this->registry));
     }
 }
